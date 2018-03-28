e08e8a414616b8913f31fb7f099c1ffbfe8eb3d5
Joanna Buchała <joanna.b2@op.pl>
2018-03-28 23:28:49 +0200

    modify js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-/*var name = prompt('Enter your name');*/
+var name = prompt('Enter your name');
 //alert('Hello, ' + name);
-/*console.log*/('Hello, ' + name);
+console.log('Hello, ' + name);
+
+//I am the danger
+
+//You know nothing
